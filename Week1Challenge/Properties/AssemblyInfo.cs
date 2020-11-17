6ffdca58d4192bdfa22623aa8572f76996d3c2b6
codyamorton <codyamorton2020@gmail.com>
2020-11-17 18:59:15 -0500

    Add project files.

---
+++ Week1Challenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week1Challenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Week1Challenge")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1ead4d3-e01e-43dc-b2b6-c0fbe3dd4605")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week1Challenge/UnitTest1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1Challenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NameAndAge()
+        {
+            string firtName = "Cody";
+            string lastName = "Morton";
+            int age = 600;
+            Console.WriteLine(age + 7);
+            Console.WriteLine(age - 7);
+            Console.WriteLine(age * 7);
+            Console.WriteLine(age / 7);
+            Console.WriteLine(age % 7);
+
+        }
+        [TestMethod]
+        public void Array()
+        {
+            string[] Movie1 = { "Batman", "Die Hard", "Inception", "Little Women" };
+        }
+
+        [TestMethod]
+        public void ListOfDates()
+        {
+            List<DateTime> listOfDates = new List<DateTime>();
+            DateTime today = DateTime.Now;
+            listOfDates.Add(today);
+            listOfDates.Add(new DateTime(2020 / 12 / 25));
+            listOfDates.Add(new DateTime(2020, 3, 12));
+            listOfDates.Add(new DateTime(2020, 6, 16));
+        }
+        {[TestMethod]
+        public void HowMuchSleepDidYouGet()
+
+        {
+            Console.WriteLine("How many hours of sleep did you get?");
+            int hours = 4;
+            if (hours >= 10)
+            {
+
+                Console.WriteLine("wow thats alot of sleep");
+            }
+            else if (hours >= 8 && hours < 10)
+            {
+                Console.WriteLine("You should be well rested");
+            }
+
+            else if (hours > 4 && hours < 8)
+            {
+                Console.WriteLine("bummer");
+            }
+            else
+            {
+                Console.WriteLine("oh man get some sleep");
+            }
+            { Console.WriteLine()};
+
+            }
+        }
+    [TestMethod]
+    public void HowHasYourDayBeen()
+    {
+        string userDay = "Okay";
+        switch (userDay)
+        {
+            case "Great":
+                Console.WriteLine("Congrats on your good day");
+                break;
+            case "Good":
+                Console.WriteLine("it is a good day");
+                   break;
+            case "Okay":
+                Console.WriteLine("Hopefully it will get better from here");
+                break;
+            case "Bad":
+                Console.WriteLine("There is always tomorrow");
+                break;
+            case ":(":
+                Console.WriteLine("I guess this is us now"); 
+                break;
+            default:
+                Console.WriteLine("Please enter answer above");
+                break;
+            } 
+          }
+    [TestMethod]
+    public void Supercalifragilisticexpialidocious()
+    {
+        string name = "Supercalifragilisticexpialidocious";
+        foreach (char letter in name)
+        {
+            Console.WriteLine(letter);
+        }
+        {
+            if (letter == 'i') ;
+            Console.WriteLine();
+
+
+            Console.WriteLine("not a i");
+                }
+            }
+        }
+    }
